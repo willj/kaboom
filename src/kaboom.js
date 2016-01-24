8480f5365bfea93c4cc4e460d1b7ebe5135ded94
Will <willj@users.noreply.github.com>
2016-01-24 17:11:40 +0000

    Tidy up random tab formatting...

--- src/kaboom.js
+++ src/kaboom.js
@@ -9,72 +9,72 @@ var kaboom = {
         
         this.canvas = Object.create(kaboomCanvas); 
         this.canvas.create(this.settings);
-                
+        
         this.gameLoopIntervalId;
-		this.actors = [];        
-		this.frameCount = 0;
-		this.approxRunTimeInSeconds = 0;
+        this.actors = [];
+        this.frameCount = 0;
+        this.approxRunTimeInSeconds = 0;
         this.gameOver = false;
     },
     startGame: function(){
         var self = this;
         this.gameLoopIntervalId = setInterval(function() {
-			self.update();
-			self.draw();	
-		}, 1000/self.settings.framesPerSecond);
+            self.update();
+            self.draw();
+        }, 1000/self.settings.framesPerSecond);
     },
     stopGame: function(){
         clearInterval(this.gameLoopIntervalId);
-		this.gameOver = true;
-		this.actors.splice(0, this.actors.length);
+        this.gameOver = true;
+        this.actors.splice(0, this.actors.length);
         if (this.gameOverCallback){
             this.gameOverCallback.apply(this);    
         }
     },
     restartGame: function(){
         clearInterval(this.gameLoopIntervalId);
-		this.actors.splice(0, this.actors.length);
-		this.draw();	
-		this.frameCount = 0;
-		this.approxRunTimeInSeconds = 0;
-		this.gameOver = false;
-		this.startGame();
+        this.actors.splice(0, this.actors.length);
+        this.draw();
+        this.frameCount = 0;
+        this.approxRunTimeInSeconds = 0;
+        this.gameOver = false;
+        this.startGame();
     },
     update: function(){
         this.frameCounter();
-		this.updateOncePerSecond();
+        this.updateOncePerSecond();
         
         if (this.updateCallback){
             this.updateCallback.apply(this);
         }
     },
     draw: function(){
-		this.canvas.context.clearRect(0, 0, this.canvas.getWidth, this.canvas.getHeight);
-
-		for (var i = 0; i < this.actors.length; i++){
-			this.actors[i].draw(this.canvas.context);
-		}
+        this.canvas.context.clearRect(0, 0, this.canvas.getWidth, this.canvas.getHeight);
+        
+        for (var i = 0; i < this.actors.length; i++){
+            this.actors[i].draw(this.canvas.context);
+        }
         
         if (this.drawCallback){
-            this.drawCallback.apply(this);    
+            this.drawCallback.apply(this);
         }
     },
     frameCounter: function(){
-		// keep a count of frames in this second and total runtime seconds so we can do things at a slower pace than fps
-		this.frameCount = (this.frameCount + 1) % this.settings.framesPerSecond;
-	},
+        // keep a count of frames in this second and total runtime seconds so we can do things at a slower pace than fps
+        this.frameCount = (this.frameCount + 1) % this.settings.framesPerSecond;
+    },
     updateOncePerSecond: function(){
-		if (this.frameCount !== 0){
-			return;
-		}
-		
-		this.approxRunTimeInSeconds += 1;
-		
+        if (this.frameCount !== 0){
+            return;
+        }
+        
+        this.approxRunTimeInSeconds += 1;
+        
         if (this.updateOncePerSecondCallback){
             this.updateOncePerSecondCallback.apply(this, [this.approxRunTimeInSeconds]);
         }
-	},
+    },
     getRandomInt: function (min, max){
-	    return Math.floor(Math.random() * (max - min)) + min;	
-	}
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
 };
--- src/kaboomcanvas.js
+++ src/kaboomcanvas.js
@@ -7,12 +7,12 @@ var kaboomCanvas = {
         if (!this.settings.canvasWidth || !this.settings.canvasHeight){
             this.detectContainerSize();
         }
-
+        
         this.canvasElement = document.createElement("canvas");
-		this.context = this.canvasElement.getContext("2d");        
+        this.context = this.canvasElement.getContext("2d");
         this.canvasElement.width = this.settings.canvasWidth;
-		this.canvasElement.height = this.settings.canvasHeight;
-		this.containerElement.appendChild(this.canvasElement);
+        this.canvasElement.height = this.settings.canvasHeight;
+        this.containerElement.appendChild(this.canvasElement);
     },
     resize: function (width, height){
         if (!width || !height){
@@ -21,9 +21,9 @@ var kaboomCanvas = {
             this.settings.canvasWidth = width;
             this.settings.canvasHeight = height;
         }
-		this.canvasElement.width = this.settings.canvasWidth;
-		this.canvasElement.height = this.settings.canvasHeight;
-	},
+        this.canvasElement.width = this.settings.canvasWidth;
+        this.canvasElement.height = this.settings.canvasHeight;
+    },
     detectContainerSize: function(){
         this.settings.canvasWidth = this.containerElement.clientWidth;
         this.settings.canvasHeight = this.containerElement.clientHeight;

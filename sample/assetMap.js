8b7cc418c61cc7686f262c12ae6ce2652e9f5543
Will <willj@users.noreply.github.com>
2016-01-27 11:33:08 +0000

    Clean up sample asset map

--- sample/assetMap.js
+++ sample/assetMap.js
@@ -1,36 +1,32 @@
 var assetMap = [];
 
 assetMap.push({
-	fileType: "Image",
-	fileName: "pigeon.png",
-	sprites: [
-			{
-				name: "leftPigeon",
-				startX: 0,
-				startY: 0,
-				width: 99,
-				height: 70,
-				hitMarginTop: 0,
-				hitMarginLeft: 0,
-				hitMarginBottom: 0,
-				hitMarginRight: 0,
-				states: {
-					initial: [0,"initial"],
-					fadeAndDie: [1,2,3,"die"]
-				}
-			},
-			{
-				name: "rightPigeon",
-				startX: 0,
-				startY: 70,
-				width: 99,
-				height: 70,
-				states: {
-					initial: [0,"initial"],
-					fadeAndDie: [1,2,3,"die"]
-				}
-			}
-		]
+    fileType: "Image",
+    fileName: "pigeon.png",
+    sprites: [
+        {
+            name: "leftPigeon",
+            startX: 0,
+            startY: 0,
+            width: 99,
+            height: 70,
+            states: {
+                initial: [0,"initial"],
+                fadeAndDie: [1,2,3,"die"]
+            }
+        },
+        {
+            name: "rightPigeon",
+            startX: 0,
+            startY: 70,
+            width: 99,
+            height: 70,
+            states: {
+                initial: [0,"initial"],
+                fadeAndDie: [1,2,3,"die"]
+            }
+        }
+    ]
 });
 
 assetMap.push({ fileType: "Sound", fileName: "quack.mp3", name: "quack" });
